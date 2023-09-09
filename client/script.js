b3e82a3ebcf0d1298ede199bfcfdd0b2afeb5764
olilue <o.m.luetolf@gmail.com>
2023-09-09 15:20:21 +0200

    first working version

--- client/script.js
+++ client/script.js
@@ -16,11 +16,44 @@ const socket = new WebSocket(backendUrl);
 // !!!!!!!!!!!! DON'T TOUCH ANYTHING ABOVE THIS LINE !!!!!!!!!!!!
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+// creates a unique id for the user
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  .replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0, 
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
+}
+
+// create default user parameters
+const userUUID = uuidv4()
+console.log(userUUID);
+const userColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+console.log(userColor);
+
+// generate an initial random username
+async function getRandomUser() {
+  const response = await fetch("https://randomuser.me/api/");
+  const data = await response.json();
+  return data.results[0];
+}
+
 socket.addEventListener("open", async (event) => {
   console.log("WebSocket connected!");
-  // TODO: create message object to transmit the user to the backend
-  const userName = document.getElementById("username").value;
-  socket.send(JSON.stringify({ type: "user", data: userName }));
+  const randomUser = await getRandomUser();
+  document.getElementById("username").value = randomUser.name.first;
+  const message = {
+    type: "user",
+    user: {
+      id: userUUID,
+      name: document.getElementById("username").value,
+      initial: document.getElementById("username").value.charAt(0),
+      color: userColor
+    },
+  };
+  console.log(JSON.stringify(message));
+  socket.send(JSON.stringify(message));
 });
 
 socket.addEventListener("message", (event) => {
@@ -29,23 +62,80 @@ socket.addEventListener("message", (event) => {
   switch (messageObject.type) {
     case "ping":
       socket.send(JSON.stringify({ type: "pong", data: "FROM CLIENT" }));
-    case "user":
-      // TODO: Show the current users as DOM elements
+    case "users":
+      showParticipants(messageObject.users);
       break;
     case "message":
-      // TODO: Show new message as DOM element append to chat history
+      console.log("work on message type message: " + messageObject.data);
+      showMessage(messageObject.data);
       break;
     default:
       console.error("Unknown message type: " + messageObject.type);
   }
 });
 
-function showUsers(users) {
+function showParticipants(users) {
   // TODO: Show the current users as DOM elements
+  const participantsElement = document.getElementById("participants-list");
+  participantsElement.innerHTML = "";
+  users.forEach((user) => {
+    const htmlParticipant = generateParticipantHTML(user);
+    const participantElement = document.createElement("li");
+    participantElement.classList.add("flex", "items-center", "space-x-3");
+    participantElement.innerHTML = htmlParticipant;
+    participantsElement.appendChild(participantElement);
+  });
 }
 
 function showMessage(message) {
-  // TODO: Show new message as DOM element append to chat history
+  console.log("show message")
+  const htmlMessage = generateMessageHTML(message.user, message.timestamp, message.message);
+  const element = document.createElement("li");
+  element.innerHTML = htmlMessage;
+  document.getElementById("chat-messages").appendChild(element);
+  const chatMessages = document.getElementById('chat-container');
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+function generateParticipantHTML(user) {
+  const html = `
+    <div class="flex-shrink-0">
+      <div class="h-3 w-3 rounded-full bg-[${user.color}]"></div>
+    </div>
+    <div class="text-sm font-medium text-white-900">${user.name}</div>
+  `
+  return html;
+}
+
+function generateMessageHTML(user, timestamp, message) {
+  console.log("generate html chat message")
+  let formattedTimestamp;
+  if (new Date().toLocaleDateString() === new Date(timestamp).toLocaleDateString()) {
+      // Same day, include only time
+      formattedTimestamp = new Date(timestamp).toLocaleTimeString();
+  } else {
+      // Not the same day, include date and time
+      formattedTimestamp = new Date(timestamp).toLocaleString();
+  }
+
+  const html = `
+  <div class="flex space-x-2 pl-2 pt-5">
+      <div class="flex-shrink-0">
+        <div 
+          class="h-10 w-10 rounded-full bg-[${user.color}] flex items-center justify-center font-bold text-white">
+          ${user.initial}</div>
+      </div>
+      <div class="flex flex-col">
+        <div class="flex items-baseline space-x-2">
+          <div id="user" class="font-bold">${user.name}</div>
+          <div class="text-sm text-gray-400">${timestamp}</div>
+        </div>
+
+        <p class="text-sm text-light-500">${message}</p>
+      </div>
+    </div>
+  `
+  return html;
 }
 
 socket.addEventListener("close", (event) => {
@@ -56,25 +146,38 @@ socket.addEventListener("error", (event) => {
   console.error("WebSocket error:", event);
 });
 
+
 function changeUsername() {
-  // TODO: Implement change username and forward new username to backend
+  const newUsername = document.getElementById("username").value;
+  if (newUsername === "") return;
+  const message = {
+    type: "user",
+    user: {
+      id: userUUID,
+      name: document.getElementById("username").value,
+      initial: document.getElementById("username").value.charAt(0),
+      color: userColor
+    },
+  };
   socket.send(JSON.stringify(message));
 }
 
 function sendMessage() {
   // TODO get message from input and send message as object to backend
-    const message = document.getElementById("message").value;
-    socket.send(JSON.stringify({
-        type: "message",
-        data: {
-          username: "oli",
-          message: message,
-          timestamp: new Date()
-        }
-    }));
-    document.getElementById("message").value = "";
-
-  socket.send(JSON.stringify(message));
+  const message = document.getElementById("message").value;
+  const messageObject = {
+      type: "message",
+      data: {
+        user: {
+          id: userUUID,
+          name: document.getElementById("username").value,
+          initial: document.getElementById("username").value.charAt(0),
+          color: userColor
+        },
+        message: message,
+        timestamp: new Date().toLocaleTimeString()
+      }
+  };
+  document.getElementById("message").value = "";
+  socket.send(JSON.stringify(messageObject));
 }
-
-document.getElementById("send-username").addEventListener("click", sendMessage);
--- server/backend.js
+++ server/backend.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws");
 const redis = require("redis");
+const expireTimeInSeconds = 15;
 let redisClient;
 
 let clients = [];
@@ -15,9 +16,71 @@ const initializeWebsocketServer = async (server) => {
   });
   await redisClient.connect();
 
+  const subscriber = redisClient.duplicate();
+  await subscriber.connect();
+  
+  publisher = redisClient.duplicate();
+  await publisher.connect();
+
   const websocketServer = new WebSocket.Server({ server });
   websocketServer.on("connection", onConnection);
   websocketServer.on("error", console.error);
+  await subscriber.subscribe("newMessage", onRedisMessage);
+
+  heartbeat();
+};
+
+const onRedisMessage = async (message) => {
+  const messageObject = JSON.parse(message);
+  console.log("Received message from redis channel: " + messageObject.type);
+  switch (messageObject.type) {
+    case "message":
+      clients.forEach((client) => {
+        client.ws.send(JSON.stringify(messageObject));
+      });
+      break;
+    case "publishUsers":
+      await publishUsers();
+      break;
+    default:
+      console.error("Unknown message type: " + messageObject.type);
+  }
+};
+
+const sendMessageHistory = async (ws) => {
+  const historyString = await getMessageHistory(); 
+  console.log("Publishing message history");
+  console.log(historyString);
+  const history = JSON.parse(historyString);
+  if (!history) return;
+  history.forEach((message) => {
+    ws.send(JSON.stringify(message));
+  });
+};
+
+const publishUsers = async () => {
+  const users = await getUsers();
+  const message = {
+    type: "users",
+    users,
+  };
+  clients.forEach((client) => {
+    client.ws.send(JSON.stringify(message));
+  });
+};
+
+const getUsers = async () => {
+  let userKeys = await redisClient.keys("user:*");
+
+  let users = [];
+  for (let i = 0; i < userKeys.length; i++) {
+    let user = await redisClient.get(userKeys[i]);
+    if (user) {
+      users.push(JSON.parse(user));
+    }
+  }
+
+  return users;
 };
 
 // If a new connection is established, the onConnection function is called
@@ -27,6 +90,8 @@ const onConnection = (ws) => {
   ws.on("message", (message) => onClientMessage(ws, message));
   // TODO: Send all connected users and current message history to the new client
   ws.send(JSON.stringify({ type: "ping", data: "FROM SERVER" }));
+  console.log("Sending message history");
+  sendMessageHistory(ws);  
 };
 
 // If a new message is received, the onClientMessage function is called
@@ -36,12 +101,31 @@ const onClientMessage = async (ws, message) => {
   switch (messageObject.type) {
     case "pong":
       console.log("Received from client: " + messageObject.data);
+      break;
     case "user":
-      // TODO: Publish all connected users to all connected clients
+      clients = clients.filter((client) => client.ws !== ws);
+      console.log("Connected clients: " + clients.length);
+      console.log("Received from client users: " + messageObject.type);
+      clients.push({ ws, user: messageObject.user });
+      redisClient.set(
+        `user:${messageObject.user.id}`,
+        JSON.stringify(messageObject.user)
+      );
+      redisClient.expire(
+        `user:${messageObject.user.id}`,
+        expireTimeInSeconds
+      );
+      const message = {
+        type: "publishUsers",
+      };
+      publisher.publish("newMessage", JSON.stringify(message));
       break;
     case "message":
       // TODO: Publish new message to all connected clients and save in redis
-      console.log("Received from client: " + messageObject.data);
+      console.log("Received message from client: " + messageObject.data.message);
+      messageHistory.push(messageObject);
+      setMessageHistory(JSON.stringify(messageHistory));
+      publisher.publish("newMessage", JSON.stringify(messageObject));
       break;
     default:
       console.error("Unknown message type: " + messageObject.type);
@@ -51,7 +135,14 @@ const onClientMessage = async (ws, message) => {
 // If a connection is closed, the onClose function is called
 const onClose = async (ws) => {
   console.log("Websocket connection closed");
-  // TODO: Remove related user from connected users and propagate new list
+  const client = clients.find((client) => client.ws === ws);
+  if (!client) return;
+  redisClient.del(`user:${client.user.id}`);
+  const message = {
+    type: "publishUsers",
+  };
+  publisher.publish("newMessage", JSON.stringify(message));
+  clients = clients.filter((client) => client.ws !== ws);
 };
 
 const getMessageHistory = async () => {
@@ -60,6 +151,14 @@ const getMessageHistory = async () => {
 
 const setMessageHistory = async (messageHistory) => {
   await redisClient.set("messageHistory", messageHistory);
+};
+
+const heartbeat = async () => {
+  for (let i = 0; i < clients.length; i++) {
+    redisClient.expire(`user:${clients[i].user.id}`, expireTimeInSeconds);
+  }
+  await publishUsers();
+  setTimeout(heartbeat, (expireTimeInSeconds * 1000) / 2);
 };
 
 module.exports = { initializeWebsocketServer };
